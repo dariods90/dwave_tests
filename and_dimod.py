dc1af5e5d51e5545120e1b2fb03c0dca33348016
dariods90 <139882460+dariods90@users.noreply.github.com>
2023-09-07 19:38:57 +0200

    and gate with dwave sampler

--- and_dimod.py
+++ and_dimod.py
@@ -2,12 +2,20 @@ from dimod.generators import and_gate
 from dimod import ExactSolver
 import numpy as np
 
+'''
 bqm = and_gate('a', 'b', 'c')
-
-#bqm = ({'a': 0.0, 'b': 0.0, 'c': 3.0}, {('b', 'a'): 1.0, ('c', 'a'): -2.0, ('c', 'b'): -2.0}, 0.0, 'BINARY')
 
 
 sampler = ExactSolver()
 sampleset = sampler.sample(bqm)
+print(sampleset)
+print(bqm)
+'''
+
+
+bqm = [[0,1,-2],[0,0,-2],[0,0,3]]
+
+sampler = ExactSolver()
+sampleset = sampler.sample_qubo(bqm)
 print(sampleset)
 print(bqm)
